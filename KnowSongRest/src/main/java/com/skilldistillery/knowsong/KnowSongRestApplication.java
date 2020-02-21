ce41a9ea4ddf3a2a9704c2df449a2b5498dd4b51
Josh-Hollopeter <joshuahollopeter@gmail.com>
2020-02-21 09:19:58 -0700

    initial commit stubbed

---
+++ KnowSongRest/src/main/java/com/skilldistillery/knowsong/KnowSongRestApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.knowsong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowSongRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowSongRestApplication.class, args);
+	}
+
+}
---
+++ KnowSongRest/src/test/java/com/skilldistillery/knowsong/KnowSongRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.knowsong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowSongRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
