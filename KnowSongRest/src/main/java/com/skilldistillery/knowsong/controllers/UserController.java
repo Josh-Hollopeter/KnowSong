eaae40e2b406d88b4485f1a49b37cd3d9aa8ab03
Neal Savant <nealrsavant@gmail.com>
2020-02-21 11:01:29 -0700

    Cross origin setup to port in user controller

---
+++ KnowSongRest/src/main/java/com/skilldistillery/knowsong/controllers/UserController.java
@@ -0,0 +1,10 @@
+package com.skilldistillery.knowsong.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin({"*", "http://localhost:4250"})
+public class UserController {
+
+}
