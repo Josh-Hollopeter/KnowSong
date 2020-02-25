174cc2f12a1eb32cb8f70bd85fe71edf98ce1a4b
George Moore <gdubya1218@gmail.com>
2020-02-25 13:47:16 -0700

    matchHistory, remove home button on userHome

--- KnowSongRest/src/main/java/com/skilldistillery/knowsong/controllers/PlaylistController.java
+++ KnowSongRest/src/main/java/com/skilldistillery/knowsong/controllers/PlaylistController.java
@@ -1,7 +1,11 @@
 package com.skilldistillery.knowsong.controllers;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +17,11 @@ import com.skilldistillery.knowsong.services.PlaylistService;
 public class PlaylistController {
 
 		@Autowired
-		private PlaylistService playlistService;
+		private PlaylistService pSvc;
+		
+		@GetMapping()
+		public void getPlaylist(Principal principal, HttpRequest request) {
+			
+			
+		}
 }
