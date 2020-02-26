4c176298584697a9082082e70a849fac2ef87e93
Neal Savant <nealrsavant@gmail.com>
2020-02-26 13:23:07 -0700

    Getting artists by search dynamically as you type. display artist photo and name. store id, name, img to artist model

--- ngKnowSong/src/app/pages/create-game/create-game.component.ts
+++ ngKnowSong/src/app/pages/create-game/create-game.component.ts
@@ -1,8 +1,8 @@
+import { Artist } from './../../spotifyJSON/models/artist';
 import { SongstreamService } from './../../spotifyJSON/services/songstream.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/models/user.service';
-
 @Component({
   selector: 'app-create-game',
   templateUrl: './create-game.component.html',
@@ -11,37 +11,72 @@ import { UserService } from 'src/app/models/user.service';
 export class CreateGameComponent implements OnInit {
 
   // F I E L D S
+
   artistStr: string;
-  searchResult: Object[];
-  user: User = new User();
+  searchResult: Artist[];
+  private user: User = new User();
+
+
   constructor(
     private stream: SongstreamService,
     private userSvc: UserService
-    ) { }
+  ) { }
+
+
+  // M E T H O D S
 
   ngOnInit(): void {
     this.userSvc.show().subscribe(
 
-    yes=>{
-      this.user.authToken = yes["authToken"];
-      // this.user.rank
-      this.user.username = yes["username"];
-      this.userSvc.setUser(this.user);
-    },
-    no=>{
-      console.error("in user home init")
-      console.error(no);
-    }
-  )
+      yes => {
+        this.user.authToken = yes["authToken"];
+        // this.user.rank
+        this.user.username = yes["username"];
+        this.userSvc.setUser(this.user);
+      },
+      no => {
+        console.error("in user home init")
+        console.error(no);
+      }
+    )
   }
 
-  searchForArtist(){
+  searchForArtist() {
     var authToken = this.userSvc.getUser().authToken;
     this.stream.searchArtist(this.artistStr, authToken).subscribe(
-      response =>{
-        // this.searchResult = response["name"];
-        console.log(response);
+      response => {
 
+        var array = response["artists"];
+        var items = array["items"];
+        // instantiate artist array to length of result
+        this.searchResult = new Array();
+
+        //get individual artist
+        for (let x = 0; x < items.length; x++) {
+          var item = items[x];
+
+          //get artist ID
+          var id = item["id"];
+
+          // get name
+          var name = item["name"];
+          console.log(item);
+
+          // get image
+          if(item["images"].length < 1){
+            var img = null;
+          }else{
+            let imgs: any[] = item["images"];
+            let firstImg = imgs[0];
+            var img = firstImg["url"];
+          }
+
+          // display artist array to user
+          var artist: Artist = new Artist(id, name, img);
+
+
+          this.searchResult.push(artist);
+        }
       }
     )
   }
--- ngKnowSong/src/app/spotifyJSON/services/songstream.service.ts
+++ ngKnowSong/src/app/spotifyJSON/services/songstream.service.ts
@@ -1,7 +1,8 @@
+import { Artist } from './../models/artist';
 import { Injectable } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { HttpClient, HttpHeaders, HttpBackend } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/models/user.service';
@@ -41,7 +42,11 @@ export class SongstreamService {
         // 'Accept': 'text/plain'
       })
     };
-    return this.http.get(url, httpOptions).pipe(
+    return this.http.get<Artist[]>(url, httpOptions).pipe(
+      tap((res) => {
+
+        return res;
+      }),
       catchError((err: any) => {
         console.log(err);
         return throwError('Could not retrieve artist from spotify API');
