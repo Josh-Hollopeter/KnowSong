e99642f0db6d301194698f148b6bbb89ce6caf00
Neal Savant <nealrsavant@gmail.com>
2020-02-27 13:43:39 -0700

    Album[] object in creategame component now populates with all tracks, and contains all albums for the chosen artist.

--- ngKnowSong/src/app/spotifyJSON/models/album.ts
+++ ngKnowSong/src/app/spotifyJSON/models/album.ts
@@ -1,30 +1,37 @@
 import { Artist } from './artist';
+import { Track } from './track';
 
 export class Album {
   id: string;
-  releaseYear: number;
+  name: string;
+  releaseDate: number;
   popularity: number;
   albumPhoto: string;
   albumType: string;
   albumGenre: string[];
   artist: Artist;
+  tracks: Track[]
 
   constructor(
     id?: string,
-    releaseYear?: number,
+    name?: string,
+    releaseDate?: number,
     popularity?: number,
     albumPhoto?: string,
     albumType?: string,
     albumGenre?: string[],
-    artist?: Artist
+    artist?: Artist,
+    tracks?: Track[]
   ) {
     this.id = id;
-    this.releaseYear = releaseYear;
+    this.name = name;
+    this.releaseDate = releaseDate;
     this.popularity = popularity;
     this.albumPhoto = albumPhoto;
     this.albumType = albumType;
     this.albumGenre = albumGenre;
     this.artist = artist;
+    this.tracks = tracks;
   }
 }
 
