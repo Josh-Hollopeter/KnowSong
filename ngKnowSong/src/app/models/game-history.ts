8b939b0a36995bf6d5679b5e19be7dfeb04c0ec7
Josh-Hollopeter <joshuahollopeter@gmail.com>
2020-03-03 09:36:38 -0700

    working on logic

---
+++ ngKnowSong/src/app/models/game-history.ts
@@ -0,0 +1,13 @@
+export class GameHistory {
+
+  public marks:number;
+  public numQuestion; number;
+
+
+  constructor(marks?:number,numQuestion?:number){
+    this.marks = marks;
+    this.numQuestion = numQuestion;
+  }
+
+
+}
