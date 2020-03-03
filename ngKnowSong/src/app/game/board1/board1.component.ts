57b0e77e29373820fb503b157cc7f8695024f687
Neal Savant <nealrsavant@gmail.com>
2020-03-03 10:33:30 -0700

    BUttons for create trivia style

--- ngKnowSong/src/app/game/board1/board1.component.ts
+++ ngKnowSong/src/app/game/board1/board1.component.ts
@@ -6,7 +6,7 @@ import { Album } from 'src/app/spotifyJSON/models/album';
 import { Quizmodel } from './../quiz/quizmodel';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import {get} from 'lodash';
+import { get } from 'lodash';
 import { JsonPipe } from '@angular/common';
 import { mapTo } from 'rxjs/operators';
 
@@ -18,18 +18,19 @@ import { mapTo } from 'rxjs/operators';
 })
 export class Board1Component implements OnInit {
 
-  //neal put fields bad neal
-  trackObject: Track;
-  albumObject: Album;
-  artistObject: Artist;
-  constructor(private router: Router, private aroute: ActivatedRoute, private data: DataService) { }
+  constructor(
+    private router: Router,
+    private aroute: ActivatedRoute,
+    private data: DataService) { }
+
   ngOnInit(): void {
     var artists = this.aroute.snapshot.paramMap.get("artists");
     this.questionBuilder();
   }
+  // F I E L D S
   myarray: String[] = [];
   i: number = 0;
-  categories: String[] = ["play clip", "Name the Albums", "Release Year"];
+  categories: String[] = ["play clip", "Name the Lyrics", "Release Year"];
   newstr: String;
   album = "Lemonade";
   year = 2014;
@@ -38,84 +39,89 @@ export class Board1Component implements OnInit {
   quizlist: Quizmodel[] = [
   ];
 
-quizlength: number;
-selectedcategory: Quizmodel[] = [];
-question: string;
-selectedvalue: String;
-option: any[];
-selectedCategories: any[];
-selected;
-playdatclip:boolean;
-correct:boolean;
-marks: number = 0;
-answerkey: AnswerKey[] = [];
-trackNames = new Array;
-trackAnswers = new Array;
+  quizlength: number;
+  selectedcategory: Quizmodel[] = [];
+  question: string;
+  selectedvalue: String;
+  option: any[];
+  selectedCategories: any[];
+  selected;
+  playdatclip: boolean;
+  correct: boolean;
+  marks: number = 0;
+  answerkey: AnswerKey[] = [];
+  trackNames = new Array;
+  trackAnswers = new Array;
 
-questionBuilder(){
-  var questionInfo = this.data.storage;
-  this.shuffle(questionInfo);
-  let j = 0;
-  let count = 0;
+  // M E T H O D S
 
-  questionInfo.forEach(element => {
-  let singer = element.artist.name;
-  let singerQuestion = "What year was " + singer + " album " + element.name + " released?";
-  var year = parseInt(element.releaseDate);
-  var years =[year+1, year -1,year+2, year];
-  years = this.shuffle(years);
-  this.quizlist.push({ID :j,category:"Release Year",question: singerQuestion,anslistobj:years,answer:year});
-  if(element.tracks){
-    this.trackNames = this.trackNames.concat(element.tracks);
-    this.trackNames = this.shuffle(this.trackNames);
+  questionBuilder() {
+    var questionInfo = this.data.storage;
+    this.shuffle(questionInfo);
+    let j = 0;
+    let count = 0;
+
+    questionInfo.forEach(element => {
+      let singer = element.artist.name;
+      let singerQuestion = "What year was " + singer + " album " + element.name + " released?";
+      var year = parseInt(element.releaseDate);
+      var years = [year + 1, year - 1, year + 2, year];
+      years = this.shuffle(years);
+      this.quizlist.push({ ID: j, category: "Release Year", question: singerQuestion, anslistobj: years, answer: year });
+      if (element.tracks) {
+        this.trackNames = this.trackNames.concat(element.tracks);
+        this.trackNames = this.shuffle(this.trackNames);
+      }
+      var test = get(element, 'track');
+
+
+    });
+    this.makeDatClip();
+    j = 0;
   }
-  var test = get(element, 'track');
+
+  makeDatClip() {
+    var ansArray = Object.assign([], this.trackNames);
+    let count = 0;
+    let questionCounter = 0;
+    for (let i = 0; i < this.trackNames.length; i++) {
+      let track = ansArray.pop();
+      console.log(track.previewUrl);
+      if (!track.previewUrl) {
+        count++
+        if (count === this.trackNames.length - 2) {
+          this.categories.shift();
+
+        }
+        continue;
+      }
+      questionCounter++
+      if (questionCounter == 7) {
+        break;
+      }
 
 
-});
-this.makeDatClip();
-  j = 0;
-}
+      this.trackAnswers = [track.name, this.trackNames[0].name, this.trackNames[0 + 1].name, this.trackNames[0 + 2].name];
+      this.quizlist.push({ ID: 0, category: "play clip", question: track.previewUrl, anslistobj: this.trackAnswers, answer: track.name });
 
-makeDatClip(){
-  var ansArray = Object.assign([], this.trackNames);
-  let count = 0;
-  let questionCounter =0;
-  for(let i =0; i < this.trackNames.length;i++){
-  let track = ansArray.pop();
-  console.log(track.previewUrl);
-  if(!track.previewUrl){
-    count++
-    if(count === this.trackNames.length-2){
-      this.categories.shift();
-
+      this.trackAnswers = this.shuffle(this.trackAnswers);
+      this.trackNames = this.shuffle(this.trackNames);
     }
-    continue;
-  }
-  questionCounter++
-  if(questionCounter == 7){
-    break;
+
   }
 
+  getQuestion() {
+    document.getElementById("my-audio").setAttribute('src', this.question);
+    return this.question;
+  }
 
-  this.trackAnswers = [track.name,this.trackNames[0].name,this.trackNames[0+1].name,this.trackNames[0+2].name];
-  this.quizlist.push({ID :0,category:"play clip",question:track.previewUrl,anslistobj:this.trackAnswers,answer:track.name});
-
-  this.trackAnswers = this.shuffle(this.trackAnswers);
-  this.trackNames = this.shuffle(this.trackNames);
-}
-
-}
-
-getQuestion(){
-  document.getElementById("my-audio").setAttribute('src', this.question);
-  return this.question;
-}
+  //CHOOSING GAME STYLE
+  //--------------------
 
   gettingCategory() {
-    if(this.selectedvalue === "play clip" ){
+    if (this.selectedvalue === "play clip") {
       this.playdatclip = true;
-    }else{
+    } else {
       this.playdatclip = false;
     }
 
@@ -130,7 +136,7 @@ getQuestion(){
     if (this.i < this.selectedCategories.length - 1) {
       ++this.i;
     }
-    if (this.i === this.selectedCategories.length-1) {
+    if (this.i === this.selectedCategories.length - 1) {
       this.roundOver = true;
     }
     this.question = this.selectedCategories[this.i].question;
@@ -145,19 +151,19 @@ getQuestion(){
   check() {
     this.correct = false;
     this.generatemark();
-    this.answerkey.push(new AnswerKey( this.selected,this.selectedCategories[this.i].answer,this.question,this.correct));
-}
-
-generatemark() {
-  if(this.selected == this.selectedCategories[this.i].answer){
-    this.marks ++;
-    this.correct = true;
+    this.answerkey.push(new AnswerKey(this.selected, this.selectedCategories[this.i].answer, this.question, this.correct));
   }
 
-}
-submit(){
-  this.roundOver = true;
-}
+  generatemark() {
+    if (this.selected == this.selectedCategories[this.i].answer) {
+      this.marks++;
+      this.correct = true;
+    }
+
+  }
+  submit() {
+    this.roundOver = true;
+  }
 
   shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -183,17 +189,14 @@ submit(){
 export class AnswerKey {
   chosen: any;
   answer: any;
-  question:any;
-  correct:boolean;
-  constructor(chosen: any, answer: any,question:any,correct:boolean) {
+  question: any;
+  correct: boolean;
+  constructor(chosen: any, answer: any, question: any, correct: boolean) {
     this.chosen = chosen;
     this.answer = answer;
     this.question = question;
     this.correct = correct;
   }
-
-
-
 }
 
 
---
+++ ngKnowSong/src/app/services/interceptor/my-jsonp-interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { MyJsonpInterceptor } from './my-jsonp-interceptor';
+
+describe('MyJsonpInterceptor', () => {
+  it('should create an instance', () => {
+    expect(new MyJsonpInterceptor()).toBeTruthy();
+  });
+});
